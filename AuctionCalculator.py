2ecf91128b1d3fe2d8b41ff836e525b755680770
Beini Ma <beinima94@gmail.de>
2021-12-14 13:12:00 +0100

    implemented actual k > 1 mechanism

--- AuctionCalculator.py
+++ AuctionCalculator.py
@@ -2,136 +2,102 @@ import numpy as np
 
 
 class AuctionCalculator:
-    def __init__(self, k=None, num_items=None, fb_values=None, sb_values=None):
-        self.k = k
+    def __init__(self, num_items=None, fb_values=None, sb_values=None):
         self.num_items = num_items
         self.fb_values = fb_values
         self.sb_values = sb_values
-        self.tree = None
+        self.sfb = None
+        self.ssb = None
+        print(sb_values)
+        print(fb_values)
+        a = self.calculate_single(k=1)
+        b = self.calculate_multiple(k=2)
+        if b > a:
+            print('Different utilities.')
+            exit()
 
-    def transform_into_single(self):
-        new_num = int((self.num_items + 1) / self.k)
-        new_fb = np.empty([new_num])
-        new_sb = np.empty([new_num])
-        for i in range(new_num):
-            new_fb[i] = sum(self.fb_values[i * self.k:(i + 1) * self.k])
-            new_sb[i] = sum(self.sb_values[i * self.k:(i + 1) * self.k])
-        return AuctionCalculator(k=1, num_items=new_num, fb_values=new_fb, sb_values=new_sb)
+    def calculate_single(self, k) -> (np.ndarray, np.ndarray):
+        ffu = np.zeros([self.num_items + 1, self.num_items + 1], dtype=int)
+        sfu = np.zeros([self.num_items + 1, self.num_items + 1], dtype=int)
 
-    def transform_test(self):
-        new_num = int((self.num_items + 1) / self.k)
-        new_fb = np.empty([new_num])
-        new_sb = np.empty([new_num])
-        for i in range(new_num):
-            new_fb[i] = sum(self.fb_values[i * self.k:(i + 1) * self.k])
-            new_sb[i] = sum(self.sb_values[i * self.k:(i + 1) * self.k])
-        single = AuctionCalculator(k=1, num_items=new_num, fb_values=new_fb, sb_values=new_sb)
-        multiple_result = self.calculate()
-        single_result = single.calculate()
-        for i in range(4):
-            if multiple_result[i] != single_result[i]:
-                print('We got a different result from our single and multiple item auction.')
-                exit()
+        self.sfb = np.zeros([self.num_items + 1, self.num_items + 1], dtype=int)
+        self.ssb = np.zeros([self.num_items + 1, self.num_items + 1], dtype=int)
 
-    def input_and_calculate(self):
-        self.input_k()
-        self.input_num_items()
-        self.fb_values = self.input_buyer_valuations(1)
-        self.sb_values = self.input_buyer_valuations(2)
-        if self.k > 1:
-            single = self.transform_into_single()
-        multiple_result = self.calculate()
-        single_result = single.calculate()
-        for i in range(4):
-            if multiple_result[i] != single_result[i]:
-                print('We got a different result from our single and multiple item auction.')
-                exit()
+        if k == 1:
+            for i in range(self.num_items - k, -1, -k):
+                for j in range(i + 1):
+                    self.sfb[i][j] = np.sum(self.fb_values[i - j:i - j + k]) + ffu[i + k][j] - \
+                               ffu[i + k][j + k]
+                    self.ssb[i][j] = np.sum(self.sb_values[j:j + k]) + sfu[i + k][j + k] - \
+                               sfu[i + k][j]
+                    if self.sfb[i][j] >= self.ssb[i][j]:
+                        ffu[i][j] = np.sum(self.fb_values[i - j:i - j + k]) + ffu[i + k][j] - self.ssb[i][j]
+                        sfu[i][j] = sfu[i + k][j]
+                    else:
+                        ffu[i][j] = ffu[i + k][j + k]
+                        sfu[i][j] = np.sum(self.sb_values[j:j + k]) + sfu[i + k][j + k] - self.sfb[i][j]
 
-    def input_k(self):
-        print('Input: Welcome to the two buyer sequential multiunit auction simulator. Please input k as an integer.')
-        self.k = int(input())
-
-    def input_num_items(self):
-        print(
-            'Input: Please enter the amount of items as an integer.')
-        while True:
-            try:
-                self.num_items = int(input())
-            except ValueError:
-                print('Error: Please enter an integer.')
-                continue
-            if self.num_items <= 0:
-                print('Error: Please enter an integer greater than 0.')
-                continue
-            return
-
-    def input_buyer_valuations(self, buyer: int) -> list:
-        example_sequence = ""
-        for i in range(self.num_items, 0, -1):
-            example_sequence += str(i) + " "
-        example_sequence = example_sequence[:-1]
-        print(
-            f'Input: The auction has {self.num_items} identical items. Please enter valuations for buyer {buyer} as a non-increasing '
-            f'sequence of integers, seperated with a blank space. As example: {example_sequence}')
-        while True:
-            try:
-                values = np.array(list((map(int, input().split()))))
-            except ValueError:
-                print('Error: Please enter only integers seperated by a blank space.')
-                continue
-            if len(values) != self.num_items:
-                print(f'Error: Please enter the correct number integers seperated by a blank space. '
-                      f'Expected were {self.num_items} integers.')
-                continue
-            if all(x <= 0 for x in values):
-                print('Error: Please enter only integers greater than 0.')
-                continue
-            failed = False
-            for i in range(len(values) - 1):
-                if values[i] < values[i + 1]:
-                    print('Error: The sequence of valuations have to be non-increasing.')
-                    failed = True
-                    break
-            if failed:
-                continue
-            return values
-
-    def print_tree(self):
-        print(self.tree)
-
-    def calculate(self) -> (np.ndarray, np.ndarray):
-        self.tree = np.zeros([self.num_items + 1, self.num_items + 1, 4], dtype=int)
-
-        for i in range(self.num_items + 1):
-            self.tree[self.num_items][i][0] = 0
-            self.tree[self.num_items][i][1] = -1
-            self.tree[self.num_items][i][2] = 0
-            self.tree[self.num_items][i][3] = -1
-
-        for i in range(self.num_items - self.k, -1, -self.k):
-            for j in range(i + 1):
-                self.tree[i][j][1] = np.sum(self.fb_values[i - j:i - j + self.k]) + self.tree[i + self.k][j][0] - self.tree[i + self.k][j + self.k][0]
-                self.tree[i][j][3] = np.sum(self.sb_values[j:j + self.k]) + self.tree[i + self.k][j + self.k][2] - self.tree[i + self.k][j][2]
-                if self.tree[i][j][1] >= self.tree[i][j][3]:
-                    self.tree[i][j][0] = np.sum(self.fb_values[i - j:i - j + self.k]) + self.tree[i + self.k][j][0] - self.tree[i][j][3]
-                    self.tree[i][j][2] = self.tree[i + self.k][j][2]
-                else:
-                    self.tree[i][j][0] = self.tree[i + self.k][j + self.k][0]
-                    self.tree[i][j][2] = np.sum(self.sb_values[j:j + self.k]) + self.tree[i + self.k][j + self.k][2] - self.tree[i][j][1]
+            self.sfb = self.sfb
+            self.ssb = self.ssb
 
         position = 0
-        for i in range(0, self.num_items, self.k):
-            print(f"In round {i}, buyer 1 bids {self.tree[i][position][1]}, buyer 2 bids {self.tree[i][position][3]}. "
-                  f"Buyer {1 if self.tree[i][position][1] >= self.tree[i][position][3] else 2} wins the item.")
-            if self.tree[i][position][1] < self.tree[i][position][3]:
-                position += self.k
+        for i in range(0, self.num_items, k):
+            print(f"In round {i}, buyer 1 bids {self.sfb[i][position]}, buyer 2 bids {self.ssb[i][position]}. "
+                  f"Buyer {1 if self.sfb[i][position] >= self.ssb[i][position] else 2} wins the item.")
+            if self.sfb[i][position] < self.ssb[i][position]:
+                position += k
 
-        fb_items = int((self.num_items - position) / self.k)
-        sb_items = int(position / self.k)
-        fb_utility = self.tree[0][0][0]
-        sb_utility = self.tree[0][0][2]
+        fb_items = int((self.num_items - position) / k)
+        sb_items = int(position / k)
+        fb_utility = ffu[0][0]
+        sb_utility = sfu[0][0]
 
         print(f"Buyer 1 won {fb_items} items and buyer 2 won {sb_items} items.")
         print(f"Buyer 1 has an utility of {fb_utility} and buyer 2 has an utility of {sb_utility}.")
 
-        return fb_items, sb_items, fb_utility, sb_utility
+        print(ffu)
+        print(sfu)
+        print(self.sfb)
+        print(self.ssb)
+
+        return fb_utility
+
+    def calculate_multiple(self, k) -> (np.ndarray, np.ndarray):
+        ffu = np.zeros([self.num_items + 1, self.num_items + 1], dtype=int)
+        sfu = np.zeros([self.num_items + 1, self.num_items + 1], dtype=int)
+
+        fb = np.zeros([self.num_items + 1, self.num_items + 1, k], dtype=int)
+        sb = np.zeros([self.num_items + 1, self.num_items + 1, k], dtype=int)
+
+        for i in range(int(self.num_items / k) - 1, -1, -1):
+            for j in range(i * k + 1):
+                s_current = 0
+                for a in range(k):
+                    sb[i * k][j][a] = self.ssb[i * k + a][j + s_current]
+                    if self.ssb[i * k][j] > self.sfb[i * k][j]:
+                        s_current += 1
+                    sb[i * k][j][::-1].sort()
+
+        for i in range(int(self.num_items / k) - 1, -1, -1):
+            for j in range(i * k + 1):
+                current_max = 0
+                max_index = 0
+                for a in range(k + 1):
+                    if a == 0:
+                        current_next = ffu[(i + 1) * k][j + (k - a)]
+                    else:
+                        current_next = ffu[(i + 1) * k][j + (k - a)] + sum(self.fb_values[(i * k) - j:(i * k) - j + a]) - (sb[i * k][j][k - a] * a)
+                    if current_next > current_max:
+                        current_max = current_next
+                        max_index = a
+                ffu[i * k][j] = current_max
+                if max_index != 0:
+                    for a in range(k):
+                        fb[i * k][j][a] = sb[i * k][j][k - max_index]
+
+        print(ffu)
+        print(fb)
+
+        fb_utility = ffu[0][0]
+
+        return fb_utility
--- main.py
+++ main.py
@@ -3,9 +3,58 @@ import numpy as np
 from AuctionCalculator import AuctionCalculator
 
 
+def input_k():
+    print('Input: Please input k as an integer to calculate the auction result.')
+    return int(input())
+
+def input_num_items():
+    print('Input: Welcome to the two buyer sequential multiunit auction simulator. Please enter the amount of items as an integer.')
+    while True:
+        try:
+            num_items = int(input())
+        except ValueError:
+            print('Error: Please enter an integer.')
+            continue
+        if num_items <= 0:
+            print('Error: Please enter an integer greater than 0.')
+            continue
+        return num_items
+
+
+def input_buyer_valuations(num_items: int, buyer: int) -> list:
+    example_sequence = ""
+    for i in range(num_items, 0, -1):
+        example_sequence += str(i) + " "
+    example_sequence = example_sequence[:-1]
+    print(
+        f'Input: The auction has {num_items} identical items. Please enter valuations for buyer {buyer} as a non-increasing '
+        f'sequence of integers, seperated with a blank space. As example: {example_sequence}')
+    while True:
+        try:
+            values = np.array(list((map(int, input().split()))))
+        except ValueError:
+            print('Error: Please enter only integers seperated by a blank space.')
+            continue
+        if len(values) != num_items:
+            print(f'Error: Please enter the correct number integers seperated by a blank space. '
+                  f'Expected were {num_items} integers.')
+            continue
+        if all(x <= 0 for x in values):
+            print('Error: Please enter only integers greater than 0.')
+            continue
+        failed = False
+        for i in range(len(values) - 1):
+            if values[i] < values[i + 1]:
+                print('Error: The sequence of valuations have to be non-increasing.')
+                failed = True
+                break
+        if failed:
+            continue
+        return values
+
+
 def test():
     num_items = 4
-    k = 2
     for i in range(10):
         for j in range(0, i + 1):
             for m in range(0, j + 1):
@@ -14,12 +63,16 @@ def test():
                         for b in range(0, a + 1):
                             for c in range(0, b + 1):
                                 for d in range(0, c + 1):
-                                    AuctionCalculator(k=k, num_items=num_items, fb_values=np.array([i, j, m, n]),
-                                                      sb_values=np.array([a, b, c, d])).transform_test()
-
+                                    AuctionCalculator(num_items=num_items, fb_values=np.array([i, j, m, n]),
+                                                      sb_values=np.array([a, b, c, d]))
 
 if __name__ == '__main__':
-    # calculator = AuctionCalculator()
-    # calculator.input_and_calculate()
+    # num_items = input_num_items()
+    # fb_values = input_buyer_valuations(num_items, 1)
+    # sb_values = input_buyer_valuations(num_items, 2)
+    # calculator = AuctionCalculator(num_items=num_items, fb_values=fb_values, sb_values=sb_values)
+    # while True:
+    #     k = input_k()
+    #     calculator.calculate_multiple(k)
 
     test()
