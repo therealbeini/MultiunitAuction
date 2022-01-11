4d7d34b23857b7a2622f19e0b5577fb227bb17f5
Beini Ma <beinima94@gmail.de>
2022-01-11 14:15:49 +0100

    iteratively compute equilibrium try #1

--- AuctionCalculator.py
+++ AuctionCalculator.py
@@ -8,13 +8,20 @@ class AuctionCalculator:
         self.sb_values = sb_values
         self.sfb = None
         self.ssb = None
-        print(sb_values)
         print(fb_values)
-        a = self.calculate_single(k=1)
-        b = self.calculate_multiple(k=2)
-        if b > a:
-            print('Different utilities.')
-            exit()
+        print(sb_values)
+        self.calculate_single(k=1)
+        _, fb_bids = self.calculate_multiple(k=2)
+        sb_utility, fb_counter_utility, sb_bids = self.find_best_response_s(2, fb_bids)
+        while True:
+            fb_utility, sb_counter_utility, fb_bids = self.find_best_response_f(2, sb_bids)
+            if fb_utility == fb_counter_utility:
+                print('Equilibrium found.')
+                break
+            sb_utility, fb_counter_utility, sb_bids = self.find_best_response_s(2, fb_bids)
+            if sb_utility == sb_counter_utility:
+                print('Equilibrium found.')
+                break
 
     def calculate_single(self, k) -> (np.ndarray, np.ndarray):
         ffu = np.zeros([self.num_items + 1, self.num_items + 1], dtype=int)
@@ -52,19 +59,18 @@ class AuctionCalculator:
         fb_utility = ffu[0][0]
         sb_utility = sfu[0][0]
 
-        print(f"Buyer 1 won {fb_items} items and buyer 2 won {sb_items} items.")
-        print(f"Buyer 1 has an utility of {fb_utility} and buyer 2 has an utility of {sb_utility}.")
-
-        print(ffu)
-        print(sfu)
-        print(self.sfb)
-        print(self.ssb)
+        # print(f"Buyer 1 won {fb_items} items and buyer 2 won {sb_items} items.")
+        # print(f"Buyer 1 has an utility of {fb_utility} and buyer 2 has an utility of {sb_utility}.")
+        #
+        # print(ffu)
+        # print(sfu)
+        # print(self.sfb)
+        # print(self.ssb)
 
         return fb_utility
 
     def calculate_multiple(self, k) -> (np.ndarray, np.ndarray):
         ffu = np.zeros([self.num_items + 1, self.num_items + 1], dtype=int)
-        sfu = np.zeros([self.num_items + 1, self.num_items + 1], dtype=int)
 
         fb = np.zeros([self.num_items + 1, self.num_items + 1, k], dtype=int)
         sb = np.zeros([self.num_items + 1, self.num_items + 1, k], dtype=int)
@@ -95,9 +101,80 @@ class AuctionCalculator:
                     for a in range(k):
                         fb[i * k][j][a] = sb[i * k][j][k - max_index]
 
-        print(ffu)
-        print(fb)
-
         fb_utility = ffu[0][0]
 
-        return fb_utility
+        return fb_utility, fb
+
+    def find_best_response_f(self, k, sb) -> (np.ndarray, np.ndarray):
+        ffu = np.zeros([self.num_items + 1, self.num_items + 1], dtype=int)
+        sfu = np.zeros([self.num_items + 1, self.num_items + 1], dtype=int)
+        fb = np.zeros([self.num_items + 1, self.num_items + 1, k], dtype=int)
+
+        for i in range(int(self.num_items / k) - 1, -1, -1):
+            for j in range(i * k + 1):
+                current_max = 0
+                max_index = 0
+                current_su = 0
+                for a in range(k + 1):
+                    if a == 0:
+                        current_next = ffu[(i + 1) * k][j + (k - a)]
+                        su = sfu[(i + 1) * k][j + (k - a)] + sum(self.sb_values[j:j + k - a]) - (fb[i * k][j][a] * (k - a))
+                    elif a != k:
+                        current_next = ffu[(i + 1) * k][j + (k - a)] + sum(self.fb_values[(i * k) - j:(i * k) - j + a]) - (sb[i * k][j][k - a] * a)
+                        su = sfu[(i + 1) * k][j + (k - a)] + sum(self.sb_values[j:j + k - a]) - (fb[i * k][j][a] * (k - a))
+                    else:
+                        current_next = ffu[(i + 1) * k][j + (k - a)] + sum(self.fb_values[(i * k) - j:(i * k) - j + a]) - (sb[i * k][j][k - a] * a)
+                        su = sfu[(i + 1) * k][j + (k - a)]
+                    if current_next > current_max:
+                        current_max = current_next
+                        current_su = su
+                        max_index = a
+                ffu[i * k][j] = current_max
+                sfu[i * k][j] = current_su
+                if max_index != 0:
+                    for a in range(k):
+                        fb[i * k][j][a] = sb[i * k][j][k - max_index]
+
+        fb_utility = ffu[0][0]
+        sb_utility = sfu[0][0]
+
+        return fb_utility, sb_utility, fb
+
+    def find_best_response_s(self, k, fb) -> (np.ndarray, np.ndarray):
+        ffu = np.zeros([self.num_items + 1, self.num_items + 1], dtype=int)
+        sfu = np.zeros([self.num_items + 1, self.num_items + 1], dtype=int)
+        sb = np.zeros([self.num_items + 1, self.num_items + 1, k], dtype=int)
+
+        for i in range(int(self.num_items / k) - 1, -1, -1):
+            for j in range(i * k + 1):
+                current_max = 0
+                max_index = 0
+                current_fu = 0
+                for a in range(k + 1):
+                    if a == 0:
+                        fu = ffu[(i + 1) * k][j + (k - a)]
+                        current_next = sfu[(i + 1) * k][j + (k - a)] + sum(self.sb_values[j:j + k - a]) - (fb[i * k][j][a] * (k - a))
+                    elif a != k:
+                        fu = ffu[(i + 1) * k][j + (k - a)] + sum(self.fb_values[(i * k) - j:(i * k) - j + a]) - (sb[i * k][j][k - a] * a)
+                        current_next = sfu[(i + 1) * k][j + (k - a)] + sum(self.sb_values[j:j + k - a]) - (fb[i * k][j][a] * (k - a))
+                    else:
+                        fu = ffu[(i + 1) * k][j + (k - a)] + sum(self.fb_values[(i * k) - j:(i * k) - j + a]) - (sb[i * k][j][k - a] * a)
+                        current_next = sfu[(i + 1) * k][j + (k - a)]
+                    if current_next > current_max:
+                        current_max = current_next
+                        current_fu = fu
+                        max_index = a
+                ffu[i * k][j] = current_fu
+                sfu[i * k][j] = current_max
+                if max_index != k:
+                    if max_index != 0:
+                        for a in range(k):
+                            sb[i * k][j][a] = fb[i * k][j][k - max_index] + 1
+                    else:
+                        for a in range(k):
+                            sb[i * k][j][a] = 1
+
+        fb_utility = ffu[0][0]
+        sb_utility = sfu[0][0]
+
+        return sb_utility, fb_utility, sb
